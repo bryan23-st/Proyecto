6422aa86782126b514a34f5cf9702ec1c54e25fc
Bryan <Bryan@LAPTOP-H6JOJ1AH>
2021-07-26 21:48:55 -0500

    conexion a base de datos del login

--- Proyecto/Form1.Designer.cs
+++ Proyecto/Form1.Designer.cs
@@ -29,19 +29,21 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txt_usuario = new System.Windows.Forms.TextBox();
-            this.txt_contraseña = new System.Windows.Forms.TextBox();
-            this.btn_ingresar = new System.Windows.Forms.Button();
-            this.btn_salir = new System.Windows.Forms.Button();
             this.btn_registarse = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_ingresar = new System.Windows.Forms.Button();
+            this.txt_contraseña = new System.Windows.Forms.TextBox();
+            this.txt_usuario = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_intentos = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.lbl_intentos);
             this.panel1.Controls.Add(this.btn_registarse);
             this.panel1.Controls.Add(this.btn_salir);
             this.panel1.Controls.Add(this.btn_ingresar);
@@ -55,23 +57,49 @@
             this.panel1.Size = new System.Drawing.Size(376, 296);
             this.panel1.TabIndex = 0;
             // 
-            // label1
+            // btn_registarse
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(129, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "BIENVENIDOS";
+            this.btn_registarse.Location = new System.Drawing.Point(134, 239);
+            this.btn_registarse.Name = "btn_registarse";
+            this.btn_registarse.Size = new System.Drawing.Size(75, 23);
+            this.btn_registarse.TabIndex = 7;
+            this.btn_registarse.Text = "Registrarse";
+            this.btn_registarse.UseVisualStyleBackColor = true;
+            this.btn_registarse.Click += new System.EventHandler(this.btn_registarse_Click);
             // 
-            // label2
+            // btn_salir
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(30, 72);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Usuario:";
+            this.btn_salir.Location = new System.Drawing.Point(205, 188);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(75, 23);
+            this.btn_salir.TabIndex = 6;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            // 
+            // btn_ingresar
+            // 
+            this.btn_ingresar.Location = new System.Drawing.Point(89, 189);
+            this.btn_ingresar.Name = "btn_ingresar";
+            this.btn_ingresar.Size = new System.Drawing.Size(75, 23);
+            this.btn_ingresar.TabIndex = 5;
+            this.btn_ingresar.Text = "Ingresar";
+            this.btn_ingresar.UseVisualStyleBackColor = true;
+            this.btn_ingresar.Click += new System.EventHandler(this.btn_ingresar_Click);
+            // 
+            // txt_contraseña
+            // 
+            this.txt_contraseña.Location = new System.Drawing.Point(132, 130);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.Size = new System.Drawing.Size(100, 20);
+            this.txt_contraseña.TabIndex = 4;
+            // 
+            // txt_usuario
+            // 
+            this.txt_usuario.Location = new System.Drawing.Point(132, 72);
+            this.txt_usuario.Name = "txt_usuario";
+            this.txt_usuario.Size = new System.Drawing.Size(100, 20);
+            this.txt_usuario.TabIndex = 3;
             // 
             // label3
             // 
@@ -82,46 +110,32 @@
             this.label3.TabIndex = 2;
             this.label3.Text = "Contraseña:";
             // 
-            // txt_usuario
+            // label2
             // 
-            this.txt_usuario.Location = new System.Drawing.Point(132, 72);
-            this.txt_usuario.Name = "txt_usuario";
-            this.txt_usuario.Size = new System.Drawing.Size(100, 20);
-            this.txt_usuario.TabIndex = 3;
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(30, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario:";
             // 
-            // txt_contraseña
+            // label1
             // 
-            this.txt_contraseña.Location = new System.Drawing.Point(132, 130);
-            this.txt_contraseña.Name = "txt_contraseña";
-            this.txt_contraseña.Size = new System.Drawing.Size(100, 20);
-            this.txt_contraseña.TabIndex = 4;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(129, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BIENVENIDOS";
             // 
-            // btn_ingresar
+            // lbl_intentos
             // 
-            this.btn_ingresar.Location = new System.Drawing.Point(89, 189);
-            this.btn_ingresar.Name = "btn_ingresar";
-            this.btn_ingresar.Size = new System.Drawing.Size(75, 23);
-            this.btn_ingresar.TabIndex = 5;
-            this.btn_ingresar.Text = "Ingresar";
-            this.btn_ingresar.UseVisualStyleBackColor = true;
-            // 
-            // btn_salir
-            // 
-            this.btn_salir.Location = new System.Drawing.Point(205, 188);
-            this.btn_salir.Name = "btn_salir";
-            this.btn_salir.Size = new System.Drawing.Size(75, 23);
-            this.btn_salir.TabIndex = 6;
-            this.btn_salir.Text = "Salir";
-            this.btn_salir.UseVisualStyleBackColor = true;
-            // 
-            // btn_registarse
-            // 
-            this.btn_registarse.Location = new System.Drawing.Point(134, 239);
-            this.btn_registarse.Name = "btn_registarse";
-            this.btn_registarse.Size = new System.Drawing.Size(75, 23);
-            this.btn_registarse.TabIndex = 7;
-            this.btn_registarse.Text = "Registrarse";
-            this.btn_registarse.UseVisualStyleBackColor = true;
+            this.lbl_intentos.AutoSize = true;
+            this.lbl_intentos.Location = new System.Drawing.Point(4, 239);
+            this.lbl_intentos.Name = "lbl_intentos";
+            this.lbl_intentos.Size = new System.Drawing.Size(35, 13);
+            this.lbl_intentos.TabIndex = 8;
+            this.lbl_intentos.Text = "label4";
             // 
             // Form1
             // 
@@ -148,6 +162,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_intentos;
     }
 }
 
--- Proyecto/Form1.cs
+++ Proyecto/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,113 @@ namespace Proyecto
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection("Data Source = LAPTOP - H6JOJ1AH\\SQLEXPRESS;Initial Catalog = Supermercado; Integrated Security = True");
+        int contador = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void login(string nombre, string pass)
+        {
+            try
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from Tbl_Usuario where usu_nomlogin = @nom and usu_password=@pas ", con);
+                cmd.Parameters.AddWithValue("nom", nombre);
+                cmd.Parameters.AddWithValue("pas", pass);
+                SqlDataAdapter sda = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                sda.Fill(dt);
+                con.Close();
+
+                if (dt.Rows.Count == 1)
+                {
+                    con.Open();
+                    SqlCommand cmd1 = new SqlCommand("select usu_nombre , tusu_id, usu_cedula from Tbl_Usuario where usu_nomlogin = @nom and usu_password = @pas", con);
+                    cmd1.Parameters.AddWithValue("nom", nombre);
+                    cmd1.Parameters.AddWithValue("pas", pass);
+                    SqlDataAdapter sda1 = new SqlDataAdapter(cmd1);
+                    DataTable dt1 = new DataTable();
+                    sda1.Fill(dt1);
+                    con.Close();
+
+                    if (dt1.Rows.Count == 1)
+                    {
+                        /*Maria-1728349385
+                          mandar el nombre de la persona q esta ingresando y la cedula al otro formulario
+                          */
+                        this.Hide();
+                        if (dt1.Rows[0][1].ToString() == "1")
+                        {
+                            MessageBox.Show("Sea bienvenido al sistema Admin " + "-" + dt1.Rows[0][0].ToString() + "-" + dt1.Rows[0][2].ToString());
+                            new principal().ShowDialog();
+                            //new Admin(dt1.Rows[0][0].ToString(), dt1.Rows[0][2].ToString()).ShowDialog(); 
+                        }
+                        else if (dt1.Rows[0][1].ToString() == "2")
+                        {
+                            MessageBox.Show("Bienvenido al sistema Usuario " + "-" + dt1.Rows[0][0].ToString() + "-" + dt1.Rows[0][2].ToString());
+                            new principal().ShowDialog();
+                            //new Usuario(dt1.Rows[0][0].ToString(), dt1.Rows[0][2].ToString()).ShowDialog();
+                        }
+                        this.Close();
+                    }
+                }
+                else
+                {
+                    con.Open();
+                    SqlCommand cmd1 = new SqlCommand("select usu_nombre , usu_estado from Tbl_Usuario where usu_nomlogin = @nom and usu_password = @pas", con);
+                    cmd1.Parameters.AddWithValue("nom", nombre);
+                    cmd1.Parameters.AddWithValue("pas", pass);
+                    SqlDataAdapter sda1 = new SqlDataAdapter(cmd1);
+                    DataTable dt1 = new DataTable();
+                    sda1.Fill(dt1);
+                    con.Close();
+
+                    MessageBox.Show("Usuario no se encuentra registrado");
+                    contador += 1;
+                    lbl_intentos.Text = contador.ToString();
+
+                    if (contador > 2)
+                    {
+                        con.Open();
+                        SqlCommand cmdo = new SqlCommand("UPDATE Tbl_Usuario SET usu_estado='B' WHERE usu_nomlogin=@nom", con);
+                        cmdo.Parameters.AddWithValue("@nom", nombre);
+                        SqlDataAdapter sdao = new SqlDataAdapter(cmdo);
+                        DataTable dto = new DataTable();
+                        sdao.Fill(dto);
+                        con.Close();
+                        MessageBox.Show("Ingreso valores erroneos en varios intentos" + "," + nombre + "," + " a sido bloqueado");
+
+                        //cmdo.Parameters.AddWithValue("@nom", nombre);
+                        //cmdo.Parameters.AddWithValue("@pas", pass);
+                        //cmdo.ExecuteNonQuery();
+                        //MessageBox.Show("El usuario a sido bloqueado");
+                        Application.Exit();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void btn_ingresar_Click(object sender, EventArgs e)
+        {
+            login(txt_usuario.Text, txt_contraseña.Text);
+        }
+
+        private void btn_registarse_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            new Registro().ShowDialog();
+            this.Close();
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
